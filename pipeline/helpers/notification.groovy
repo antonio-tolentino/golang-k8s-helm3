07efa9475023286a29b12590c6a2d0de267dcd66
Antonio Tolentino de Castro Junior <atcjunior@gmail.com>
2020-11-09 16:46:57 -0300

    test external lib

--- pipeline/helpers/notification.groovy
+++ pipeline/helpers/notification.groovy
@@ -1,12 +1,13 @@
     
 def sendSuccess() {
     // success email template
-    emailTemplate = "${env.WORKSPACE}/pipeline/email/successful_deploy_body_html.tpl"
+    emailTemplate = "${WORKSPACE}/pipeline/email/successful_deploy_body_html.tpl"
+    slackTemplate = "${WORKSPACE}/pipeline/slack/successful_deploy.md"
 
     // email body replace
     echo 'INFO: Replacing successful email template tokens'
     sh "sed -i 's/{APP_NAME}/${APP_NAME}/g' ${emailTemplate}"
-    sh "sed -i 's/{JOB_NAME}/${JOB_NAME.replace('/','\\/').replace('%2F','\\/')}/g' ${env.WORKSPACE}/pipeline/${emailTemplate}"
+    sh "sed -i 's/{JOB_NAME}/${JOB_NAME.replace('/','\\/').replace('%2F','\\/')}/g' ${emailTemplate}"
     sh "sed -i 's/{BRANCH_NAME}/${BRANCH_NAME.replace('/','\\/')}/g' ${emailTemplate}"
     sh "sed -i 's/{BUILD_URL}/${BUILD_URL.replace('/','\\/')}/g' ${emailTemplate}"
     sh "sed -i 's/{RUN_DISPLAY_URL}/${RUN_DISPLAY_URL.replace('/','\\/')}/g' ${emailTemplate}"
@@ -27,13 +28,13 @@ def sendSuccess() {
     if (SLACK_CHANNEL != "" && SLACK_CREDENTIAL_ID != "" && SLACK_DOMAIN != ""){
         // Slack template replace
         echo 'INFO: Replacing start build slack template tokens'
-        sh "sed -i 's/{APP_NAME}/${APP_NAME}/g' pipeline/slack/successful_deploy.md"
-        sh "sed -i 's/{JOB_NAME}/${JOB_NAME.replace('/','\\/').replace('%2F','\\/')}/g' pipeline/slack/successful_deploy.md"
-        sh "sed -i 's/{BRANCH_NAME}/${BRANCH_NAME.replace('/','\\/')}/g' pipeline/slack/successful_deploy.md"
-        sh "sed -i 's/{BUILD_URL}/${BUILD_URL.replace('/','\\/')}/g' pipeline/slack/successful_deploy.md"
-        sh "sed -i 's/{RUN_DISPLAY_URL}/${RUN_DISPLAY_URL.replace('/','\\/')}/g' pipeline/slack/successful_deploy.md"
-        sh "sed -i 's/{DURATION_STRING}/${currentBuild.durationString.replace(' and counting', '')}/g' pipeline/slack/successful_deploy.md"
-        def slackMsg = readFile(file: 'pipeline/slack/successful_deploy.md')
+        sh "sed -i 's/{APP_NAME}/${APP_NAME}/g' ${slackTemplate}"
+        sh "sed -i 's/{JOB_NAME}/${JOB_NAME.replace('/','\\/').replace('%2F','\\/')}/g' ${slackTemplate}"
+        sh "sed -i 's/{BRANCH_NAME}/${BRANCH_NAME.replace('/','\\/')}/g' ${slackTemplate}"
+        sh "sed -i 's/{BUILD_URL}/${BUILD_URL.replace('/','\\/')}/g' ${slackTemplate}"
+        sh "sed -i 's/{RUN_DISPLAY_URL}/${RUN_DISPLAY_URL.replace('/','\\/')}/g' ${slackTemplate}"
+        sh "sed -i 's/{DURATION_STRING}/${currentBuild.durationString.replace(' and counting', '')}/g' ${slackTemplate}"
+        def slackMsg = readFile(file: '${slackTemplate}')
         //slack notification
         slackSend channel: "${SLACK_CHANNEL}", 
                   tokenCredentialId:"${SLACK_CREDENTIAL_ID}", 
