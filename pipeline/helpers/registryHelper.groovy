59493d65279439e5750e941053ae39b034d99775
Antonio Tolentino de Castro Junior <atcjunior@gmail.com>
2020-12-16 13:47:21 -0300

    test registry

---
+++ pipeline/helpers/registryHelper.groovy
@@ -0,0 +1,28 @@
+def defineRegistry(){
+    
+    def registry
+
+    switch (BRANCH_NAME) {
+        case 'develop': 
+            registry = registryDevelop(); 
+            break;
+        case ~/^release\/.*/: 
+            registry = registryRelease(); 
+            break;
+        case 'master': 
+            registry = registryProd(); 
+            break;
+        default: 
+            registry = 'tst';
+    }
+
+    return registry
+}
+
+def registryDevelop(){
+    return ${DEVELOP_GCP_PROJECT}
+}
+
+
+
+return this
